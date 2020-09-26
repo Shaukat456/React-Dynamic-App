f378dc9fa2f75d203d8fa3c2173c380eb9693ca6
Shaukat <sohailriazz267@gmail.com>
2020-09-26 08:39:48 +0500

    FIRST REACT APP

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const first = 'DARK'
+const second = '13 REASONS'
+const third = 'cosmos'
+const fourth = 'Breaking Bad'
+// <>
+//   <h1>NETFLIX RECOMMANDATIONS </h1>
+//   <p>HERE ARE 4 OF MY TOP  LISTED SHOWS</p>
+//   <ol>
+//     <li> {first} </li>
+//     <li>{second}
+//     </li>  <li>{third}
+//     </li> <li> {fourth}</li>
+//   </ol>
+// </>,
+var cdate=new Date();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.Fragment>   
+  <h1 contentEditable='true' > SHOUKAT</h1>
+  <button>Submit</button>
+  <img src='https://picsum.photos/200/300'/> 
+  </React.Fragment>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
