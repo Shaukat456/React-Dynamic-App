f2b441724b49aae6b4f0c05a680ee72b9f7fa784
Shaukat <sohailriazz267@gmail.com>
2020-10-24 10:18:26 +0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,29 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import Reactdom from 'react-dom'
+import Form from './Form'
+import App from './Form'
 
-const first = 'DARK'
-const second = '13 REASONS'
-const third = 'cosmos'
-const fourth = 'Breaking Bad'
-// <>
-//   <h1>NETFLIX RECOMMANDATIONS </h1>
-//   <p>HERE ARE 4 OF MY TOP  LISTED SHOWS</p>
-//   <ol>
-//     <li> {first} </li>
-//     <li>{second}
-//     </li>  <li>{third}
-//     </li> <li> {fourth}</li>
-//   </ol>
-// </>,
-var cdate=new Date();
 
-ReactDOM.render(
-  <React.Fragment>   
-  <h1 contentEditable='true' > SHOUKAT</h1>
-  <button>Submit</button>
-  <img src='https://picsum.photos/200/300'/> 
-  </React.Fragment>,
-  document.getElementById('root')
-);
-
+Reactdom.render(
+    <App/>,
+    document.getElementById('root')
+)
---
+++ src/links.jsx
@@ -0,0 +1,28 @@
+const sdata=[
+    {
+        sname:'DARK',
+        imgsrc:'https://unsplash.com/wallpapers/colors/dark',
+        title:'A Netflix original series',
+        srcc:'google.com'
+    },
+    {
+        sname:'breaking bad',
+        imgsrc:'https://unsplash.com/wallpapers/colors/dark',
+        title:'A Netflix original series',
+        srcc:'facebook.com'
+    },
+    {
+        sname:'cosmos',
+        imgsrc:'https://unsplash.com/wallpapers/colors/dark',
+        title:'A Netflix original series',
+        srcc:'linkedin.com'
+    },
+    {
+        sname:'13 reasons why',
+        imgsrc:'https://unsplash.com/wallpapers/colors/dark',
+        title:'A Netflix original series',
+        srcc:'twitter.com'
+    }
+    
+]
+export default sdata;
