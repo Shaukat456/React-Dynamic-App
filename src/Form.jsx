f32ec33bac3c81ad1da703a6e382dc666fd9f10d
Shaukat <sohailriazz267@gmail.com>
2020-10-26 14:34:00 +0500

    Initial Commit

--- src/Form.jsx
+++ src/Form.jsx
@@ -3,35 +3,28 @@ import { useState } from 'react'
 import css from './index.css'
 
 
-const App = () => {
+const App=()=>{
 
-    
+    //defining state
+    const state=useState();
+    const [name,setname]=useState()
+    const [fullname, setfullname]=useState()
 
-    const [name, setname] = useState()
-    const [fullname, setfullname] = useState()
-    // const [email, setemail] = useState('')
-
-
-
-    const InputEvent = (v) => {
+    const inputfun=(e)=>{
+        setname(e.target.value)
+    }
+    const click=()=>{
         setfullname(name)
-        setname(v.target.value)
-        // setemail(v.target.value)
     }
-    const onSubmits = (event) => {
-        event.preventDefault();
-        setfullname(name);
-    }
-    return (
+     
+    return(
+        <>
         <div style={css}>
-            <form onSubmit={onSubmits}>
-            <h1 > HELLO  {fullname}</h1>
-            <input type='text' placeholder='enter name' onChange={InputEvent} value={name} />
-            {/* <input type='text' placeholder='enter Email' onChange={InputEvent} value={email} /> */}
-            <button type='submit'>Click me</button>
-            </form>
+        <h1> Hi {fullname}</h1>
+        <input  onChange={inputfun} type="text" value={name} />
+        <button onClick={click} >CLick me </button>
         </div>
+        </>    
     )
 }
-
 export default App;
